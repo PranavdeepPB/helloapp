d0493bef32b68c26ea9ff38fd6ca15c5d6a38434
PranavdeepPB <kumarbindhuranipradeep@gmail.com>
2026-05-23 11:16:16 +0530

    Code Modified

--- app/index.js
+++ app/index.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, World!</h1>');
+  res.send(`
+    <h1>Hello, World!</h1>
+    <h2>Version 2 deployed automatically via GitHub Actions on code push!</h2>
+  `);
 });
 
 app.listen(PORT, () => {
